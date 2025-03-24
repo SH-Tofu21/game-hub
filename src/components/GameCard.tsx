a18b5e73c7a8ee417c6a10ce75af99b0329f9a95
SH-Tofu <chris001.educat@gmail.com>
2025-03-25 05:48:26 +0800

    Display platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,6 +11,12 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+
+        {/* {game.parent_platforms.map(({ platform }) => (
+          <Text>{platform.name}</Text>
+        ))}
+        ; */}
+
         <PlatformIconList
           platforms={game.parent_platforms.map((p) => p.platform)}
         />
