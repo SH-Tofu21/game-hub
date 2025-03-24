f1065a5e13a580c501e2cce82004f1c0dce24a9e
SH-Tofu <chris001.educat@gmail.com>
2025-03-25 03:25:10 +0800

    Create custom hook to fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,27 +1,49 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
+// import { useEffect, useState } from "react";
+// import apiClient from "../services/api-client";
+// import { Text } from "@chakra-ui/react";
+
+// interface Game {
+//   id: number;
+//   name: string;
+// }
+
+// interface FetchGamesResponse {
+//   count: number;
+//   results: Game[];      // Array of game objects
+// }
+
+// const GameGrid = () => {
+//   const [games, setGames] = useState<Game[]>([]);
+//   const [error, setError] = useState("");
+
+//   useEffect(() => {
+//     apiClient
+//       .get<FetchGamesResponse>("/games")
+//       .then(({ data: games }) => setGames(games.results))
+//       .catch((err) => setError(err.message));
+//   }, []);
+
+//   return (
+//     <>
+//       {error && <Text>{error}</Text>}
+//       <ul>
+//         {games.map((game) => (
+//           <li key={game.id}>{game.name}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
+
+// export default GameGrid;
+
+// -------------------------------------------------------------------------------------------------//
+
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[]; // Array of game objects
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then(({ data: games }) => setGames(games.results))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
