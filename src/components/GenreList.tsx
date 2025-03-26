1fd3336d5bbd2491f19ffc89b49ea82ad85b817e
SH-Tofu <chris001.educat@gmail.com>
2025-03-26 23:11:35 +0800

    Highlight selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -64,9 +64,10 @@ import getCroppedImageURL from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (isLoading) return <Spinner />;
@@ -86,6 +87,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
             {/* <Text fontSize="lg">{genre.name}</Text> */}
             {/* <Button onClick={() => console.log(genre)} fontSize="lg">{genre.name}></Button> */}
             <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               fontSize="lg"
               variant="link"
